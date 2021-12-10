35a8a6b7ba08cee42c7705c1fc8e37d5819cdd2d
ivymagesti <61289027+ivymagesti@users.noreply.github.com>
2021-12-10 11:27:18 -0300

    Create Main.java

---
+++ for_DIO/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i;
+
+        for (i=1; i<=11;i=i+2){
+            if (i == 7){
+                System.out.println("   ");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
